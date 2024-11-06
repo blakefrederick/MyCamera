fd47059f28416662125b8589afd0715889a4bafa
Blake Frederick <bf@six12creative.com>
2024-11-05 23:27:25 -0800

    this works

--- MyCamera/CameraModel.swift
+++ MyCamera/CameraModel.swift
@@ -48,13 +48,13 @@ class CameraModel: NSObject, ObservableObject, AVCapturePhotoCaptureDelegate {
     }
     
     func takePhoto() {
-        sessionQueue.async {
-            let settings = AVCapturePhotoSettings()
-            self.photoOutput.capturePhoto(with: settings, delegate: self)
-        }
+        let settings = AVCapturePhotoSettings()
+        self.photoOutput.capturePhoto(with: settings, delegate: self)
     }
     
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+    func photoOutput(_ output: AVCapturePhotoOutput,
+                     didFinishProcessingPhoto photo: AVCapturePhoto,
+                     error: Error?) {
         if let error = error {
             print("Error capturing photo: \(error.localizedDescription)")
             return
@@ -68,51 +68,91 @@ class CameraModel: NSObject, ObservableObject, AVCapturePhotoCaptureDelegate {
         
         // Process the image to apply overlays and borders
         let processedImage = processImage(uiImage: uiImage)
-
+        
         savePhotoToLibrary(uiImage: processedImage)
     }
     
-func processImage(uiImage: UIImage) -> UIImage {
-    let imageSize = uiImage.size
-    let squareLength = min(imageSize.width, imageSize.height)
-    let x = (imageSize.width - squareLength) / 2
-    let y = (imageSize.height - squareLength) / 2
-    let cropRect = CGRect(x: x, y: y, width: squareLength, height: squareLength)
-    
-    guard let cgImage = uiImage.cgImage?.cropping(to: cropRect) else {
-        print("Failed to crop image.")
-        return uiImage
+    func processImage(uiImage: UIImage) -> UIImage {
+        // Create a UIView with the same size as the image
+        let imageSize = uiImage.size
+        let view = UIView(frame: CGRect(origin: .zero, size: imageSize))
+        
+        // Create an UIImageView with the image
+        let imageView = UIImageView(image: uiImage)
+        imageView.frame = CGRect(origin: .zero, size: imageSize)
+        view.addSubview(imageView)
+        
+        // Create the overlay as in ContentView.swift
+        let overlayView = UIView(frame: view.bounds)
+        overlayView.backgroundColor = UIColor.black.withAlphaComponent(0.925)
+        
+        // Create the transparent center 
+        let centerRectWidth = imageSize.width * 1.0 
+        let centerRectHeight = imageSize.width * 0.8 
+        let centerRectX = (imageSize.width - centerRectWidth) / 2
+        let centerRectY = (imageSize.height - centerRectHeight) / 2
+        let centerRect = CGRect(x: centerRectX, y: centerRectY, width: centerRectWidth, height: centerRectHeight)
+        
+        // Create a mask layer to make the center transparent
+        let maskLayer = CAShapeLayer()
+        let path = UIBezierPath(rect: overlayView.bounds)
+        path.append(UIBezierPath(rect: centerRect).reversing())
+        maskLayer.path = path.cgPath
+        overlayView.layer.mask = maskLayer
+        view.addSubview(overlayView)
+        
+        addLShapedCorners(to: view, centerRect: centerRect)
+        
+        // Render the view to an image
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, uiImage.scale)
+        guard let context = UIGraphicsGetCurrentContext() else {
+            print("Failed to create graphics context.")
+            return uiImage
+        }
+        view.layer.render(in: context)
+        let processedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return processedImage ?? uiImage
     }
     
-    let croppedImage = UIImage(cgImage: cgImage, scale: uiImage.scale, orientation: uiImage.imageOrientation)
-    
-    // Create a new image context to draw  the corners and transparent overlay
-    UIGraphicsBeginImageContextWithOptions(croppedImage.size, false, uiImage.scale)
-    guard let context = UIGraphicsGetCurrentContext() else {
-        print("Failed to create graphics context.")
-        return croppedImage
+    func addLShapedCorners(to view: UIView, centerRect: CGRect) {
+        let lineWidth: CGFloat = 2.0
+        let cornerLength: CGFloat = 30.0
+        let strokeColor = UIColor.white.withAlphaComponent(0.7)
+        
+        // Top-left corner
+        let topLeftCorner = UIView(frame: CGRect(x: centerRect.minX, y: centerRect.minY, width: cornerLength, height: lineWidth))
+        topLeftCorner.backgroundColor = strokeColor
+        let topLeftVertical = UIView(frame: CGRect(x: 0, y: 0, width: lineWidth, height: cornerLength))
+        topLeftVertical.backgroundColor = strokeColor
+        topLeftCorner.addSubview(topLeftVertical)
+        view.addSubview(topLeftCorner)
+        
+        // Top-right corner
+        let topRightCorner = UIView(frame: CGRect(x: centerRect.maxX - cornerLength, y: centerRect.minY, width: cornerLength, height: lineWidth))
+        topRightCorner.backgroundColor = strokeColor
+        let topRightVertical = UIView(frame: CGRect(x: cornerLength - lineWidth, y: 0, width: lineWidth, height: cornerLength))
+        topRightVertical.backgroundColor = strokeColor
+        topRightCorner.addSubview(topRightVertical)
+        view.addSubview(topRightCorner)
+        
+        // Bottom-left corner
+        let bottomLeftCorner = UIView(frame: CGRect(x: centerRect.minX, y: centerRect.maxY - lineWidth, width: cornerLength, height: lineWidth))
+        bottomLeftCorner.backgroundColor = strokeColor
+        let bottomLeftVertical = UIView(frame: CGRect(x: 0, y: -cornerLength + lineWidth, width: lineWidth, height: cornerLength))
+        bottomLeftVertical.backgroundColor = strokeColor
+        bottomLeftCorner.addSubview(bottomLeftVertical)
+        view.addSubview(bottomLeftCorner)
+        
+        // Bottom-right corner
+        let bottomRightCorner = UIView(frame: CGRect(x: centerRect.maxX - cornerLength, y: centerRect.maxY - lineWidth, width: cornerLength, height: lineWidth))
+        bottomRightCorner.backgroundColor = strokeColor
+        let bottomRightVertical = UIView(frame: CGRect(x: cornerLength - lineWidth, y: -cornerLength + lineWidth, width: lineWidth, height: cornerLength))
+        bottomRightVertical.backgroundColor = strokeColor
+        bottomRightCorner.addSubview(bottomRightVertical)
+        view.addSubview(bottomRightCorner)
     }
-    
-    // Draw the cropped image
-    croppedImage.draw(at: CGPoint.zero)
-    
-    let overlayColor = UIColor.black.withAlphaComponent(0.925)
-    let imageWidth = croppedImage.size.width
-    let imageHeight = croppedImage.size.height
-    
-    // Draw top and bottom overlays (25% of the image height)
-    let overlayHeight = imageHeight * 0.25
-    context.setFillColor(overlayColor.cgColor)
-    context.fill(CGRect(x: 0, y: 0, width: imageWidth, height: overlayHeight))
-    context.fill(CGRect(x: 0, y: imageHeight - overlayHeight, width: imageWidth, height: overlayHeight))
-    
-    // Get the processed image
-    let processedImage = UIGraphicsGetImageFromCurrentImageContext()
-    UIGraphicsEndImageContext()
-    
-    return processedImage ?? croppedImage
-}
-
     
     private func savePhotoToLibrary(uiImage: UIImage) {
         PHPhotoLibrary.requestAuthorization { status in
